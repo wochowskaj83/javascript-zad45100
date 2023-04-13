189d83fa51a8174e4eaec4d1179c4a3f123cf574
wochowskaj83 <wochowskaj@gmail.com>
2023-04-13 16:17:02 +0200

    rozwiazanie zadania

---
+++ main.js
@@ -0,0 +1,11 @@
+let mefirst = "Uwielbiam JavaScript";
+let meIntroduce = "Jestem świetną programistką";
+
+function myself(a, b) {
+   if ("a">"b") {
+    return a;
+}else{
+    return b;
+}}
+
+console.log(myself(mefirst, meIntroduce))
