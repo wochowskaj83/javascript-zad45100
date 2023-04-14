a72f95dcde4e2426011fdce6b59cca3733720445
wochowskaj83 <wochowskaj@gmail.com>
2023-04-14 14:01:07 +0200

    z poprawka

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let mefirst = "Uwielbiam JavaScript";
 let meIntroduce = "Jestem świetną programistką";
 
 function myself(a, b) {
-   if ("a">"b") {
+   if (a.length>b.length) {
     return a;
 }else{
     return b;
